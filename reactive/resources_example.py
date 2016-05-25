4ed9371c11091fbdb8f45816d77ea22554c2c80b
Matt Bruzek <matthew.bruzek@canonical.com>
2016-05-25 15:20:33 -0500

    Updated code for this example.

--- reactive/resources_example.py
+++ reactive/resources_example.py
@@ -1,7 +1,17 @@
-from charms.reactive import when, when_not, set_state
+#!/usr/bin/env python
+
+import subprocess
+from charmhelpers.core.hookenv import resource_get
+from charms.reactive import when
+from charms.reactive import when_not
+from charms.reactive import set_state
 
 
-@when_not('terms-resources.installed')
-def install_terms-resources():
-
-    set_state('terms-resources.installed')
+@when_not('resources-example.fetched')
+def fetch_resources_example():
+    """ Fetch the resource from Juju."""
+    print('Fetching resource with resource_get(name="software")'')
+    path = resource_get(name='software')
+    print(path)
+    subprocess.check_call(['sha1sum', path])
+    set_state('resources-example.fetched')
